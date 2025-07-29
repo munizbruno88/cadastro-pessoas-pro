3f1c00e24394290529ebab7ea502b47374acf86b
Bruno <bgmuniz.23@gmail.com>
2025-07-29 19:41:13 -0300

    feat:Cadastro de multiplas pessoas e separado por função

--- main.go
+++ main.go
@@ -14,41 +14,64 @@ type Pessoa struct {
 	Altura float64
 }
 
+var scanner = bufio.NewScanner(os.Stdin)
+
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	var p Pessoa
+
+	CadastarPessoa(Pessoa{})
+
+}
+
+func CadastarPessoa(p Pessoa) {
+	var opcao string
+	var pessoas []Pessoa
 	fmt.Println("Sistema de Cadastro de Pessoas")
+	for {
+		//--Cdastra nome--
+		fmt.Print("Digite o nome: ")
+		scanner.Scan()
+		p.Nome = scanner.Text()
 
-	//--Cdastra nome--
-	fmt.Print("Digite o nome: ")
-	scanner.Scan()
-	p.Nome = scanner.Text()
+		//--Cadastra idade--
+		fmt.Print("Digite a idade: ")
+		scanner.Scan()
+		idadeStr := scanner.Text()
+		idade, err := strconv.Atoi(idadeStr)
+		if err != nil {
+			os.Exit(0)
+		}
+		p.Idade = idade
 
-	//--Cadastra idade--
-	fmt.Print("Digite a idade: ")
-	scanner.Scan()
-	idadeStr := scanner.Text()
-	idade, err := strconv.Atoi(idadeStr)
-	if err != nil {
-		os.Exit(0)
+		//--Cadastra CPF--
+		fmt.Print("Digite o CPF: ")
+		scanner.Scan()
+		p.CPF = scanner.Text()
+
+		//--Cadastra Altura--
+		fmt.Print("Digite a altura: ")
+		scanner.Scan()
+		alturaStr := scanner.Text()
+		altura, err := strconv.ParseFloat(alturaStr, 64)
+		if err != nil {
+			os.Exit(0)
+		}
+		p.Altura = altura
+		//--Adicionando a pessoa no slice
+		pessoas = append(pessoas, p)
+		fmt.Print("Deseja continuar cadastrando? s/n: ")
+		scanner.Scan()
+		opcao = scanner.Text()
+
+		if opcao == "s" {
+			continue
+		} else if opcao == "n" {
+			//--lendo os dados do slice
+			for _, p := range pessoas {
+				fmt.Println("\nPessoas Cadastradas")
+				fmt.Printf("Nome: %s\nIdade: %danos\nCPF: %s\nAltura: %.2fm ", p.Nome, p.Idade, p.CPF, p.Altura)
+				os.Exit(0)
+			}
+		}
+
 	}
-	p.Idade = idade
-
-	//--Cadastra CPF--
-	fmt.Print("Digite o CPF: ")
-	scanner.Scan()
-	p.CPF = scanner.Text()
-
-	//--Cadastra Altura--
-	fmt.Print("Digite a altura: ")
-	scanner.Scan()
-	alturaStr := scanner.Text()
-	altura, err := strconv.ParseFloat(alturaStr, 64)
-	if err != nil {
-		os.Exit(0)
-	}
-	p.Altura = altura
-
-	fmt.Println(p)
-
 }
